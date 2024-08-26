2971d2808fc5a468777cd2131de32bd751ab5e39
lhc0506 <lhc0506@gmail.com>
2024-08-26 22:57:06 +0900

    chore: Install twin.macro

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+      babel: {
+        plugins: [
+          "babel-plugin-macros",
+          [
+            "@emotion/babel-plugin-jsx-pragmatic",
+            {
+              export: "jsx",
+              import: "__cssprop",
+              module: "@emotion/react",
+            },
+            "css-prop",
+          ],
+          [
+            "@emotion/babel-plugin-jsx-pragmatic",
+            {
+              export: "Fragment",
+              import: "__fragment",
+              module: "react/jsx-runtime",
+            },
+            "fragment",
+          ],
+          ["@babel/plugin-transform-react-jsx", { pragma: "__cssprop", pragmaFrag: "__fragment" }],
+        ],
+      },
+    }),
+  ],
+});
