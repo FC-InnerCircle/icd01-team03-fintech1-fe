8f2025e1fae4547c766d96fddee5c293072586f1
lhc0506 <lhc0506@gmail.com>
2024-09-09 21:11:20 +0900

    [feat] 결제 프로세스 메인 컴포넌트 추가

---
+++ src/components/PaymentProcess.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import CardPaymentForm from "./CardPaymentForm";
+import { PaymentFormData } from "../types";
+import PaymentMethodSelection from "./PaymentMethodSelection";
+
+type PaymentStep = "method-selection" | "card-payment";
+
+type PaymentProcessProps = {
+  onSubmit: (data: PaymentFormData) => void;
+};
+
+const PaymentProcess: React.FC<PaymentProcessProps> = ({ onSubmit }) => {
+  const [currentStep, setCurrentStep] = useState<PaymentStep>("method-selection");
+  const [, setPaymentMethod] = useState<"card" | "bank" | null>(null);
+
+  const handleMethodSelect = (method: "card" | "bank") => {
+    setPaymentMethod(method);
+    if (method === "card") {
+      setCurrentStep("card-payment");
+    } else {
+      console.log("Bank transfer selected");
+    }
+  };
+
+  const handleCardPaymentSubmit = (data: PaymentFormData) => {
+    onSubmit(data);
+  };
+
+  return (
+    <div className="max-w-md p-6 mx-auto mt-10 bg-white rounded-lg shadow-md">
+      {currentStep === "method-selection" && <PaymentMethodSelection onSelect={handleMethodSelect} />}
+      {currentStep === "card-payment" && <CardPaymentForm onSubmit={handleCardPaymentSubmit} />}
+    </div>
+  );
+};
+
+export default PaymentProcess;
